149880e36cdd5273a5f7e31c709afead66081ed0
georg0n <abashkin.george@yandex.ru>
2024-12-01 23:05:03 +0300

    added Third task

---
+++ secondTask/secondTask/src/ru/mirea/lab3/Author.java
@@ -0,0 +1,34 @@
+package ru.mirea.lab2;
+
+public class Author {
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String newName, String newEmail, char newGender){
+        name = newName;
+        email = newEmail;
+        gender = newGender;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(String newEmail){
+        email = newEmail;
+    }
+
+    public char getGender(){
+        return gender;
+    }
+
+    public String toString(){
+        return name + " (" + gender + ") at " + email;
+    }
+
+}
---
+++ secondTask/secondTask/src/ru/mirea/lab3/TestAuthor.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab2;
+
+public class TestAuthor {
+    public static void main(String[] args){
+        Author b1 = new Author("Sergey", "sergey@yandex.ru", 'M');
+        System.out.println(b1);
+        b1.setEmail("sergey123@gmail.com");
+        System.out.println(b1);
+    }
+}
